5b64618891ccb3a765835c69c7bd6349a372ddd8
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-26 17:13:33 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -48,7 +48,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_id)
+    params.require(:song).permit(:title)
   end
   
 
